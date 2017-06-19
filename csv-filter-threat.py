74038f498dbdbd695837c2a039f4f01550649b99
Adam <amaynard@clarku.edu>
2017-06-19 11:47:03 -0400

    Update csv-filter-threat.py

--- csv-filter-threat.py
+++ csv-filter-threat.py
@@ -1,4 +1,12 @@
 #!/usr/bin/env python3
+
+### Copyright 2017 Adam Maynard
+### Licensed under the Apache License, Version 2.0 (the "License");
+### you may not use this file except in compliance with the License.
+### You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+
+# Build for PAN-OS 7.1
+
 ## Export Palo Alto threat log as csv, then use this to filter the junk
 import pandas as pd
 #Rename your csv file and remove ".csv" if you want to append later
